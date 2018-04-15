1021d914af7609dfcce40e2d8f30e83755d9d618
Jamie Scott <github@jami.org.uk>
2018-04-15 20:02:12 +0100

    Added logging to main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # DEVELOPMENT TESTING #
 
+import logging
 from sensor_net import SensorNetwork, Node
+
+logging.basicConfig(format='%(asctime)s[%(levelname)8s][%(module)s] %(message)s', datefmt='[%m/%d/%Y][%I:%M:%S %p]')
+logger = logging.getLogger(__name__)
 
 sn = SensorNetwork('/dev/ttyUSB0')
 sn.discover(5)
